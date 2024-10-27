e4a14bd2e2462c3376837e55ca0420f6f73eed4b
shristigg <165525105+shristigg@users.noreply.github.com>
2024-10-27 14:57:43 -0400

    Adding files for Lab 6

--- Shristi_Giri_Lab6-JS.js
+++ Shristi_Giri_Lab6-JS.js
@@ -1,4 +1,4 @@
-async function createMap() {
+function createMap() {
     /** set-up guide mapping creation */
     const map = L.map('map').setView([38, -96], 4); 
 
@@ -13,13 +13,20 @@ async function createMap() {
 
         L.marker([latitude, longitude]).addTo(map);
 
-        const locality = await getLocality(latitude, longitude);
-
-        document.getElementById('markers-coordinate').innerHTML += `
-            <p>
-                <b><h3>Marker ${i}</b>: Latitude: ${latitude}, Longitude: ${longitude}<h3>
-                <h4>Locality: ${locality}<h4>
-            </p>`;
+        getLocality(latitude, longitude).then(locality => {
+            document.getElementById('markers-coordinate').innerHTML += `
+                <p>
+                    <b><h3>Marker ${i}</b>: Latitude: ${latitude}, Longitude: ${longitude}<h3>
+                    <h4>Locality: ${locality}<h4>
+                </p>`;
+        }).catch(error => {
+            console.error('Error fetching locality:', error);
+            document.getElementById('markers-coordinate').innerHTML += `
+                <p>
+                    <b><h3>Marker ${i}</b>: Latitude: ${latitude}, Longitude: ${longitude}<h3>
+                    <h4>Locality: Unknown<h4>
+                </p>`;
+        });
     }
 }
 
@@ -28,7 +35,7 @@ function getRandomInRange(from, to, fixed) {
     return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
 }
 
-/** function to fecth locality data from API */
+/** function to fetch locality data from API */
 function getLocality(latitude, longitude) {
     return fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
         .then(response => response.json())
